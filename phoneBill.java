8c7ea67889952e196824620dac5b487c2059cc9c
somerscyn <144164003+somerscyn@users.noreply.github.com>
2024-02-07 15:55:32 -0500

    Update phoneBill.java

--- phoneBill.java
+++ phoneBill.java
@@ -1,12 +1,55 @@
-// Read the README.md
-// Read it again
-// Your code here
-// Delete these 4 lines of comments
+import java.util.Scanner;
+import java.text.DecimalFormat;
 
-class HelloWorld {
-    public static void main(String[] args) {
+public class Main
+{
+	public static void main(String[] args) {
+	 Scanner input = new Scanner(System.in);
+	  DecimalFormat formatter = new DecimalFormat("###,###,###.##");
 
-        System.out.println("Phone Bill Activity!");
-        
-    }
+	    double cost = 0;
+	    
+	   System.out.println("What is your phone package? Valid options are green, blue, purple:");
+	   String plan = input.nextLine().toLowerCase();
+	   
+	   while (plan != green || plan != blue || plan != purple){
+	   System.out.println("Invalid input. What is your phone package? Valid options are green, blue, purple:");
+	   String plan = input.nextLine().toLowerCase();
+	   }
+	   
+	   System.out.println("How many gb did you use? Input a number");
+	   int gb = input.nextInt();
+	   
+	   if (plan == "green"){
+	   System.out.println("Do you have a coupon? Answer yes or no.");
+	   String coupon = input.nextLine().toLowerCase();
+	       
+    	    while (coupon != "yes" || coupon != "no" ){
+    	   System.out.println("Invalid input. Do you have a coupon? Answer yes or no.");
+    	   String plan = input.nextLine().toLowerCase();
+    	    }
+	        
+	        cost = (gb - 2)*15 + 49.99;
+	        
+	        if (coupon == "yes" && cost = 75){
+	        cost -=20;
+	        }
+     
+	   } // end of green
+	   
+	   
+	   if (plan == "blue"){
+	       cost = (gb - 4)*10 + 70;
+	   }
+	   
+	   if (plan == "purple"){
+	       cost = 99.95;}
+
+	
+	}
+	
+    System.out.println( "You used " + gb + "GB, and it cost you $" + formatter.format(cost));
+	
+	
+
 }
